a60b4ef1c46a925170357ba4adb7674d830d2744
Tanner <tannersiciliano@gmail.com>
2019-06-12 10:22:54 -0700

    created a separate search_db function

--- djsearch/utils.py
+++ djsearch/utils.py
@@ -1,3 +1,10 @@
+from functools import reduce
+import operator
+
+from django.apps import apps
+from django.db.models import Q
+from django.http import JsonResponse
+
 def django_search_serializer(obj):
     if 'link' in obj.djresult_output.keys():
         link = obj.djresult_output['link']
@@ -11,7 +18,37 @@ def django_search_serializer(obj):
         'link': link,
         'fields': {}
     }
-    
+
     for f in obj.djresult_output['fields']:
         serialized_data['fields'][f] = obj.__dict__[f]
     return serialized_data
+
+def search_db(query, format = 'dict', models = apps.get_models()):
+    context = {
+        'results':{}
+    }
+
+    # Loop through all models in INSTALLED_APPS
+    for m in models:
+        # Checks if the 'search_fields' attribute is defined.
+        # If the 'search_fields' attribute is not defined, it will not search
+        # the model
+        if hasattr(m, 'djsearch_fields'):
+            filter_kwargs = []
+
+            # initialize an empty queryset for the model in question (m)
+            qs = m.objects.none()
+
+            # go through each field
+            for s in m.djsearch_fields:
+
+                # split words in search query to search for each one individually
+                for word in query.split(' '):
+                    filter_kwargs.append(Q(**{s + '__contains': word}))
+                    qs = qs | m.objects.filter(reduce(operator.or_, filter_kwargs))
+            if format != 'queryset':
+                for i in qs.distinct():
+                    context['results'][i.__str__()] = django_search_serializer(i)
+    if format == 'queryset':
+        return qs.distinct()
+    return context
--- djsearch/views.py
+++ djsearch/views.py
@@ -1,38 +1,9 @@
 from functools import reduce
 import operator
 
-from django.shortcuts import render
-from django.conf import settings
-from django.apps import apps
-from django.db.models import Q
-from django.http import JsonResponse
-from .utils import django_search_serializer
+from .utils import django_search_serializer, search_db
 
 def search(request):
     q = request.GET['q']
-    models = apps.get_models()
-    context = {
-        'results':{}
-    }
-
-    # Loop through all models in INSTALLED_APPS
-    for m in models:
-        # Checks if the 'search_fields' attribute is defined.
-        # If the 'search_fields' attribute is not defined, it will not search
-        # the model
-        if hasattr(m, 'djsearch_fields'):
-            filter_kwargs = []
-
-            # initialize an empty queryset for the model in question (m)
-            qs = m.objects.none()
-
-            # go through each field
-            for s in m.djsearch_fields:
-
-                # split words in search query to search for each one individually
-                for word in q.split(' '):
-                    filter_kwargs.append(Q(**{s + '__contains': word}))
-                    qs = qs | m.objects.filter(reduce(operator.or_, filter_kwargs))
-            for i in qs.distinct():
-                context['results'][i.__str__()] = django_search_serializer(i)
+    context = search_db(q)
     return JsonResponse(context)
